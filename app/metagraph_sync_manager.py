05cc5283d44ad7cabebfd29a5e142aa5b62589f5
dmose <100089505+janusdotai@users.noreply.github.com>
2025-10-10 15:55:59 -0400

    testing longer timeouts

--- app/metagraph_sync_manager.py
+++ app/metagraph_sync_manager.py
@@ -1,15 +1,72 @@
+import gc
 import time
 import logging
 import traceback
 import multiprocessing
-import gc  # Added for explicit cleanup
 from dotenv import load_dotenv
 load_dotenv()
 from typing import Dict, Any, Tuple
-from fiber.chain import interface
+#from fiber.chain import interface
 from fiber.chain.metagraph import Metagraph
+from async_substrate_interface import SubstrateInterface
+from fiber import constants as fcst
+
 
 logger = logging.getLogger(__name__)
+
+
+class FiberLoader:
+
+        # def _get_chain_endpoint(self, subtensor_network: str | None, subtensor_address: str | None) -> str:
+        #     if subtensor_network is None and subtensor_address is None:
+        #         raise ValueError("subtensor_network and subtensor_address cannot both be None")
+
+        #     if subtensor_address is not None:
+        #         logger.info(f"Using chain address: {subtensor_address}")
+        #         return subtensor_address
+
+        #     if subtensor_network not in fcst.SUBTENSOR_NETWORK_TO_SUBTENSOR_ADDRESS:
+        #         raise ValueError(f"Unrecognized chain network: {subtensor_network}")
+
+        #     subtensor_address = fcst.SUBTENSOR_NETWORK_TO_SUBTENSOR_ADDRESS[subtensor_network]
+        #     logger.info(f"Using the chain network: {subtensor_network} and therefore chain address: {subtensor_address}")
+        #     return subtensor_address
+
+        @staticmethod
+        def get_substrate_with_timeout( 
+            subtensor_network: str | None = fcst.FINNEY_NETWORK,
+            subtensor_address: str | None = None,
+            retry_timeout: int = 120
+        ) -> SubstrateInterface:
+            
+            def _get_chain_endpoint(subtensor_network: str | None, subtensor_address: str | None) -> str:
+                if subtensor_network is None and subtensor_address is None:
+                    raise ValueError("subtensor_network and subtensor_address cannot both be None")
+
+                if subtensor_address is not None:
+                    logger.info(f"Using chain address: {subtensor_address}")
+                    return subtensor_address
+
+                if subtensor_network not in fcst.SUBTENSOR_NETWORK_TO_SUBTENSOR_ADDRESS:
+                    raise ValueError(f"Unrecognized chain network: {subtensor_network}")
+
+                subtensor_address = fcst.SUBTENSOR_NETWORK_TO_SUBTENSOR_ADDRESS[subtensor_network]
+                logger.info(f"Using the chain network: {subtensor_network} and therefore chain address: {subtensor_address}")
+                return subtensor_address
+
+
+            subtensor_address = _get_chain_endpoint(subtensor_network, subtensor_address)
+
+            substrate = SubstrateInterface(
+                ss58_format=42,
+                use_remote_preset=True,
+                url=subtensor_address,
+                retry_timeout=retry_timeout
+            )
+            logger.info(f"Connected to {subtensor_address}")
+
+            return substrate
+
 
 class MetagraphSyncManager:
     """Dedicated manager to keep metagraph data fresh without leaking threads."""
@@ -23,6 +80,7 @@ class MetagraphSyncManager:
         self._stop_event = multiprocessing.Event()
         self._process: multiprocessing.Process | None = None
         self._queue: multiprocessing.Queue = multiprocessing.Queue()
+      
 
     def start(self) -> None:
         if self._process and self._process.is_alive():
@@ -62,7 +120,11 @@ class MetagraphSyncManager:
             substrate = None
             tmp_metagraph = None
             try:
-                substrate = interface.get_substrate(subtensor_network=network)
+                #substrate = interface.get_substrate(subtensor_network=network)
+                retry_timeout = 180
+                logger.info(f"Fiber custom loader with timeout {retry_timeout} seconds")
+                substrate = FiberLoader.get_substrate_with_timeout(subtensor_network=network, retry_timeout=retry_timeout)
+
                 tmp_metagraph = Metagraph(
                     netuid=netuid,
                     substrate=substrate,
