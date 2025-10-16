6e40ac1664ffe636380152970728e68cd965f0a1
dmose <100089505+janusdotai@users.noreply.github.com>
2025-10-16 10:14:10 -0400

    updated test

--- tests/test_verify_payload.py
+++ tests/test_verify_payload.py
@@ -1,18 +1,38 @@
+import hashlib
 import os
+import secrets
 import httpx
 import json
 import base64
 import pytest
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 from dotenv import load_dotenv
 load_dotenv()
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
+from fiber.chain import chain_utils, interface
+from fiber import (
+    Keypair
+)
 
 BASE_URL = "http://localhost:8000"
 #BASE_URL = "https://verified.bitrecs.ai"
 
-HOTKEY = os.environ.get("HOTKEY")
 OPENROUTER_KEY = os.environ.get("OPENROUTER_KEY")
+
+
+def sign_verified_request(miner_keypair: Keypair, provider: str, payload: dict) -> Tuple[str, str]:
+    nonce = secrets.token_hex(16)    
+    payload_str = json.dumps({
+        "hotkey": miner_keypair.ss58_address,
+        "provider": provider,
+        "nonce": nonce,
+        "payload": payload
+    }, separators=(',', ':'), sort_keys=True)
+    payload_hash = hashlib.sha256(payload_str.encode('utf-8')).digest()    
+    signature_bytes = miner_keypair.sign(payload_hash)
+    signature_hex = signature_bytes.hex()
+    return signature_hex, nonce
+
 
 async def get_public_key() -> Ed25519PublicKey:
     """Get public key from proxy server."""    
@@ -24,25 +44,57 @@ async def get_public_key() -> Ed25519PublicKey:
         return Ed25519PublicKey.from_public_bytes(raw_bytes)
         
 
-async def call_proxy_server(
+# async def call_proxy_server(
+#     request: dict, 
+#     openrouter_key: str, 
+#     hotkey: str
+# ) -> Dict[str, Any]:
+#     """Call the proxy server with a request."""
+#     headers = {
+#         "Authorization": f"Bearer {openrouter_key}",
+#         "x-hotkey": hotkey,
+#         "x-provider": "OPEN_ROUTER"
+#     }
+#     async with httpx.AsyncClient(timeout=30.0) as client:
+#         response = await client.post(
+#             f"{BASE_URL}/v1/chat/completions",
+#             json=request,
+#             headers=headers
+#         )
+#         print(f"Response status code: {response.status_code}")
+#         print(f"Response content: {response.text}")
+#         response.raise_for_status()
+#         return response.json()
+    
+
+async def call_proxy_server_with_signing(
     request: dict, 
     openrouter_key: str, 
-    hotkey: str
+    hotkey: str,
+    keypair: Keypair
 ) -> Dict[str, Any]:
     """Call the proxy server with a request."""
     headers = {
         "Authorization": f"Bearer {openrouter_key}",
         "x-hotkey": hotkey,
-        "x-provider": "OPEN_ROUTER"
+        "x-provider": "OPEN_ROUTER"        
     }
+    signature, nonce = sign_verified_request(keypair, "OPEN_ROUTER", request)
+    headers["x-signature"] = signature
+    headers["x-nonce"] = nonce
+
     async with httpx.AsyncClient(timeout=30.0) as client:
         response = await client.post(
             f"{BASE_URL}/v1/chat/completions",
             json=request,
             headers=headers
         )
+        print(f"Response status code: {response.status_code}")
+        print(f"Response content: {response.text}")
         response.raise_for_status()
         return response.json()
+
+
 
 def verify_signature(
     response: Dict[str, Any], 
@@ -65,21 +117,43 @@ def verify_signature(
         print(f"Verification failed: {e}")
         return False
 
-@pytest.mark.asyncio
-async def test_verify_signature():
-    """Fetch the public key, make request, sign, then verify."""
-    public_key = await get_public_key()
+# @pytest.mark.asyncio
+# async def test_verify_signature():
+#     """Fetch the public key, make request, sign, then verify."""
+#     public_key = await get_public_key()
 
+#     request = {
+#         "model": "gpt-4o-mini",
+#         "messages": [{"role": "user", "content": "Hello, how are you?"}]
+#     }
+
+#     # Returns a SignedResponse object with openrouter package, proof, signature
+#     response = await call_proxy_server(request, OPENROUTER_KEY, HOTKEY)
+#     print(f"Response: {response}")
+
+#     assert verify_signature(response, public_key), "Signature verification failed"
+
+
+
+@pytest.mark.asyncio
+async def test_verify_signature_with_signing():
+    """Fetch the public key, make request, sign, then verify."""  
+    wallet_name = os.environ.get("TEST_WALLET_NAME")
+    wallet_hotkey = os.environ.get("TEST_WALLET_HOTKEY")
+    
+    miner_keypair = chain_utils.load_hotkey_keypair(wallet_name, wallet_hotkey)
+    assert miner_keypair is not None, "Failed to load miner_keypair"
+
+    public_key = await get_public_key()
     request = {
         "model": "gpt-4o-mini",
         "messages": [{"role": "user", "content": "Hello, how are you?"}]
     }
 
     # Returns a SignedResponse object with openrouter package, proof, signature
-    response = await call_proxy_server(request, OPENROUTER_KEY, HOTKEY)
+    response = await call_proxy_server_with_signing(request, OPENROUTER_KEY, miner_keypair.ss58_address, miner_keypair)
     print(f"Response: {response}")
 
     assert verify_signature(response, public_key), "Signature verification failed"
-
 
 
