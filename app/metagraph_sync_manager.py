d0975ec291fe9fb6d4436370803ee4cf03248d76
dmose <100089505+janusdotai@users.noreply.github.com>
2025-11-05 08:18:20 -0500

    DIE engine updates

--- app/metagraph_sync_manager.py
+++ app/metagraph_sync_manager.py
@@ -13,45 +13,6 @@ from fiber.chain.metagraph import Metagraph
 logger = logging.getLogger(__name__)
 
 
-# class FiberLoader:
-
-        
-#         @staticmethod
-#         def get_substrate_with_timeout( 
-#             subtensor_network: str | None = fcst.FINNEY_NETWORK,
-#             subtensor_address: str | None = None,
-#             retry_timeout: int = 120
-#         ) -> SubstrateInterface:
-            
-#             def _get_chain_endpoint(subtensor_network: str | None, subtensor_address: str | None) -> str:
-#                 if subtensor_network is None and subtensor_address is None:
-#                     raise ValueError("subtensor_network and subtensor_address cannot both be None")
-
-#                 if subtensor_address is not None:
-#                     logger.info(f"Using chain address: {subtensor_address}")
-#                     return subtensor_address
-
-#                 if subtensor_network not in fcst.SUBTENSOR_NETWORK_TO_SUBTENSOR_ADDRESS:
-#                     raise ValueError(f"Unrecognized chain network: {subtensor_network}")
-
-#                 subtensor_address = fcst.SUBTENSOR_NETWORK_TO_SUBTENSOR_ADDRESS[subtensor_network]
-#                 logger.info(f"Using the chain network: {subtensor_network} and therefore chain address: {subtensor_address}")
-#                 return subtensor_address
-
-
-#             subtensor_address = _get_chain_endpoint(subtensor_network, subtensor_address)
-
-#             substrate = SubstrateInterface(
-#                 ss58_format=42,
-#                 use_remote_preset=True,
-#                 url=subtensor_address,
-#                 retry_timeout=retry_timeout
-#             )
-#             logger.info(f"Connected to {subtensor_address}")
-
-#             return substrate
-
-
 class MetagraphSyncManager:
     """Dedicated manager to keep metagraph data fresh without leaking threads."""
     def __init__(self, network: str, netuid: int, sync_interval: int = 600, max_cycles_before_restart: int = 8):
